a96bb414b19eb9b57d9f4aa1e9aeb3ae924d9adf
tadimadhukrishna123-commits <tadimadhukrishna123@gmail.com>
2026-03-20 11:11:47 +0530

    Update faceauth.js

--- www/faceauth.js
+++ www/faceauth.js
@@ -1,43 +1,12 @@
 var exec = require('cordova/exec');
 
-exports.startAadhaar = function (mobile, success, error) {
+exports.faceAuth = function (salt, success, error) {
 
-    var cred = JSON.stringify({
-        CredAllowed: [{
-            type: "BIOMETRIC",
-            subtype: "AADHAR_NUMBER_AUTH"
-        }]
-    });
-
-    var salt = JSON.stringify({
-        appId: "com.bank.app",
-        credType: ["aadharNumberAuth"],
-        deviceId: "",
-        mobileNumber: mobile,
-        txnId: ["TXN123456"],
-        random: Math.random().toString(36).substring(2)
-    });
-
-    exec(success, error, "FaceAuthPlugin", "startAadhaar", [cred, salt]);
-};
-
-exports.faceAuth = function (mobile, success, error) {
-
-    var cred = JSON.stringify({
-        CredAllowed: [{
-            type: "BIOMETRIC",
-            subtype: "FACE_AUTH"
-        }]
-    });
-
-    var salt = JSON.stringify({
-        appId: "com.bank.app",
-        credType: ["faceAuth"],
-        deviceId: "",
-        mobileNumber: mobile,
-        txnId: ["TXN123456"],
-        random: Math.random().toString(36).substring(2)
-    });
-
-    exec(success, error, "FaceAuthPlugin", "faceAuth", [cred, salt]);
+    exec(
+        success,
+        error,
+        "FaceAuthPlugin",
+        "faceAuth",
+        [salt]
+    );
 };
